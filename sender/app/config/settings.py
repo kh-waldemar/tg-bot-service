6ea79dcdd4e14078737eac87cc750aa09ca6e61a
kh-waldemar <62740051+kh-waldemar@users.noreply.github.com>
2025-06-18 22:52:58 +0300

    Refactor for userbot and webhook support

--- sender/app/config/settings.py
+++ sender/app/config/settings.py
@@ -10,14 +10,15 @@ class Settings(BaseSettings):
 
     PROJECT_NAME: str = 'tg-bot-service-sender'
 
-    TG_BOT_TOKEN: str = 'tg-bot-token'
     TG_API_ID: str = 'tg-api-id'
     TG_API_HASH: str = 'tg-api-hash'
     TG_SESSION_NAME: str = 'tg_session_sender'
+    TG_PHONE_NUMBER: str = '+10000000000'
 
     TG_FILES_CHAT_ID: int = 0
 
     class Config:
+        env_file = '.env'
         case_sensitive = True
 
 
