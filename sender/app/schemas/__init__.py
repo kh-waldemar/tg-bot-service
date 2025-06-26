50f1aa808175b5f51cd14370a0539213b5033134
kh-waldemar <62740051+kh-waldemar@users.noreply.github.com>
2025-06-26 15:15:33 +0300

    fix receiver media volume

--- sender/app/schemas/__init__.py
+++ sender/app/schemas/__init__.py
@@ -1,1 +1,5 @@
 from .user import PlainMessageSend, DocumentCreate, DocumentSend
+from .bot import (
+    SendMessage, SendPhoto, SendDocument, SendAudio,
+    SendVoice, SendVideo, EditMessageText, DeleteMessage
+)
